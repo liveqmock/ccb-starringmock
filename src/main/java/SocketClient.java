ccdcb55c49f4e80b4c764c283130100bb0366858
zhanrui <zhanrui@gmail.com>
2014-01-09 20:02:09 +0800

    init

--- src/main/java/SocketClient.java
+++ src/main/java/SocketClient.java
@@ -12,12 +12,12 @@ import java.util.Date;
  * Date: 13-11-25
  * Time: 上午11:49
  */
-public class StarMock {
+public class SocketClient {
 
     private String ip;
     private int port;
 
-    public StarMock(String ip, int port) {
+    public SocketClient(String ip, int port) {
         this.ip = ip;
         this.port = port;
     }
@@ -61,16 +61,9 @@ public class StarMock {
         return recvbuf;
     }
 
-
-    public static void main(String... argv) throws UnsupportedEncodingException {
-        StarMock mock = new StarMock("127.0.0.1", 60010);
-
-        String header;
-        String body;
-
-/*
-        //1561070 入资登记预交易
-        header = "1.0" +
+    //1561070 入资登记预交易
+    private String getRequestMsg1070(){
+        String header = "1.0" +
                 "123456789012345678" +
                 "0000" +
                 "1561070" +
@@ -80,11 +73,13 @@ public class StarMock {
                 "AICQDE" +
                 new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) +
                 "MAC12345678901234567890123456789";
-        body = "02|1111|22|12345678901234567890123456789012|";
-*/
+        String body = "02|1111|22|12345678901234567890123456789012|";
+        return header+body;
+    }
 
-        //1561010  入资登记
-        header = "1.0" +
+    //1561010  入资登记
+    private String getRequestMsg1010(){
+        String header = "1.0" +
                 "123456789012345678" +
                 "0000" +
                 "1561010" +
@@ -94,7 +89,7 @@ public class StarMock {
                 "AICQDE" +  //appid 6
                 new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) +
                 "MAC12345678901234567890123456789";
-        body = "02|" +  //银行代码
+        String body = "02|" +  //银行代码
                 "1111|" +  //地区码
                 "22|" +   //工商局编号
                 "12345678901234567890123456789012|" +  //预登记号	32	CHAR	右补空格
@@ -108,15 +103,71 @@ public class StarMock {
                 "张三2,80101111222,123.45,20131212,出资人开户行,370101199707070030|" + //出资人名称,出资人帐号,出资金额,入资日期,出资人开户行,证件号
                 "张三3,80101111222,123.45,20131212,出资人开户行,370101199707070030|" + //出资人名称,出资人帐号,出资金额,入资日期,出资人开户行,证件号
                 "";
+        return header+body;
+    }
 
-        String message = header + body;
+    //1561071 增资登记预交易
+    private String getRequestMsg1071(){
+        String header = "1.0" +
+                "123456789012345678" +
+                "0000" +
+                "1561070" +
+                "999999999" +
+                "123456789012" +
+                "STAR01" +
+                "AICQDE" +
+                new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) +
+                "MAC12345678901234567890123456789";
+        String body = "02|1111|22|12345678901234567890123456789012|";
+        return header+body;
+    }
+
+    //1561011  增资登记
+    private String getRequestMsg1011(){
+        String header = "1.0" +
+                "123456789012345678" +
+                "0000" +
+                "1561010" +
+                "999999999" +
+                "123456789012" +
+                "STAR01" +  //userid 6
+                "AICQDE" +  //appid 6
+                new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) +
+                "MAC12345678901234567890123456789";
+        String body = "02|" +  //银行代码
+                "1111|" +  //地区码
+                "22|" +   //工商局编号
+                "12345678901234567890123456789012|" +  //预登记号	32	CHAR	右补空格
+                "工商局名称|" + //工商局名称	32	CHAR	右补空格
+                "8010000011111111|" + //入资帐号	22	CHAR	右补空格
+                "1234567.89|" + //入资帐户余额	21.2	CHAR	左补空格
+                "企业预核准名称|" + //企业预核准名称	72	CHAR	右补空格
+                "入资银行名称|" + //入资银行名称	50	CHAR	右补空格
+                "3|" + //出资人循环个数	4	CHAR	右补空格
+                "张三1,80101111222,123.45,20131212,出资人开户行,370101199707070030|" + //出资人名称,出资人帐号,出资金额,入资日期,出资人开户行,证件号
+                "张三2,80101111222,123.45,20131212,出资人开户行,370101199707070030|" + //出资人名称,出资人帐号,出资金额,入资日期,出资人开户行,证件号
+                "张三3,80101111222,123.45,20131212,出资人开户行,370101199707070030|" + //出资人名称,出资人帐号,出资金额,入资日期,出资人开户行,证件号
+                "";
+        return header+body;
+    }
+
+    public static void main(String... argv) throws UnsupportedEncodingException {
+        System.out.println("" + argv.length);
+        if (argv.length <= 3) {
+            System.out.println("命令参数示例：\n" +
+                    "    MockClient -ip=127.0.0.1 -port=60004 -txn=1010");
+            System.exit(1);
+        }
+
+        SocketClient mock = new SocketClient("127.0.0.1", 60004);
+
+        String message = mock.getRequestMsg1010();
+
         int len = message.getBytes("GBK").length;
         String strLen = "" + (len + 6);
-
         for (int i = strLen.length(); i < 6; i++) {
             strLen += " ";
         }
-
         byte[] recvbuf = mock.call((strLen + message).getBytes("GBK"));
         System.out.printf("服务器返回报文：%s\n", new String(recvbuf, "GBK"));
     }
